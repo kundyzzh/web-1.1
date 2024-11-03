e273ed64d051454348b7f19f69c2a47a682b988c
kundyzzh <167603149+kundyzzh@users.noreply.github.com>
2024-11-03 18:24:28 +0500

    Add files via upload

---
+++ web2/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from models import db, User, Portfolio
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+    portfolios = Portfolio.query.all()
+    return render_template('portfolio.html', portfolios=portfolios)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        else:
+            return "Invalid credentials"
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
